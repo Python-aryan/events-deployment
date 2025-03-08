4a71e753e2c9c62375bddcf8b637912c05a9a57b
Python-aryan <9classhomeworksnotes1@gmail.com>
2025-03-08 22:19:16 +0530

    Updated by updating the server files in client directory.

--- client/api/models/likesEvent.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const LikeSchema = new mongoose.Schema({
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event", required: true },
-  visitorId: { type: String, required: true }, // Unique identifier from FingerprintJS
-}, { timestamps: true });
-
-export default mongoose.model("Like", LikeSchema);
